ff60117ccba546257f0bcd425bd02d0d4956b351
aayushraj07 <aayush.97raj@gmail.com>
2020-04-20 01:04:37 +0530

    jinja template used ,creating a small json database

--- flashcards.py
+++ flashcards.py
@@ -1,10 +1,18 @@
-from flask import Flask
+# from datetime import datetime
+from flask import Flask, render_template
+
+from model import db
 
 app = Flask(__name__)
-#this calls flask constructor which creates a global Flask application object
+# this calls flask constructor which creates a global Flask application object
 
+# this line makes it a view function , this is a decorator
 @app.route("/")
-#this line makes it a view function , this is a decorator
-
 def welcome():
-    return "Welcome to my Flash Cards application"
+    return render_template("welcome.html",
+                           message="Here is a message from the view")
+
+@app.route("/card")
+def card_view():
+    card = db[3]
+    return render_template("card.html",card=card)
